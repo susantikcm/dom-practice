e51e3b724f97938b0349736f1d034cfddf2002db
susantikcm <susantikcm@yahoo.com>
2020-09-10 17:04:42 +1000

    submit 4 tasks

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 // Buttons
+document.querySelector("#display-name-button").addEventListener('click', () => {
+    document.querySelector("#display-name").textContent = "Susanti";
+    document.querySelector("#display-name").classList.toggle('invisible');
+});
 
 // Mouse Over
-const toggleLight = document.querySelector("#light-bulb");
-toggleLight.onmouseenter = () => {
-    toggleLight.src = "images/light-bulb-on.png";
-}
-toggleLight.onmouseleave = () => {
-    toggleLight.src = "images/light-bulb-off.png";
-}
+let light = document.querySelector("#light-bulb");
+
+light.onmouseenter = () => light.src = "images/light-bulb-on.png";
+light.onmouseleave = () => light.src = "images/light-bulb-off.png";
 
 // Toggle
-function toggleAlert() {
-    let toggleAlert = document.querySelector("#toggle-alert");
-    let alertButton = document.querySelector("#toggle-button");
-    if (toggleAlert.innerHTML === "Hello World!") {
-        (toggleAlert.innerHTML = " ") && (alertButton.innerHTML = "Off");
-    } else {
-        (toggleAlert.innerHTML = "Hello World!") && (alertButton.innerHTML = "On");
-    }
-}
+let toggleAlert = () => document.querySelector("#toggle-alert").classList.toggle('invisible');
 
 // Validate
+document.querySelector('#form-validate button').addEventListener('click', () => {
+    event.preventDefault();
 
+    let input = document.querySelector("#form-validate-first-name");
+    if (input.value.length >= 2) {
+        input.classList.add('is-valid');
+        input.classList.remove('is-invalid');
+    }
+    else {
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+    }
+});
 
 // Challenge: Lists
+
+
 
 // Challenge: Lists (Part 2)
