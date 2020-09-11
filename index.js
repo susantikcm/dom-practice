75c909025312a52a39bc6412ad133b31d19e3ec4
susantikcm <susantikcm@yahoo.com>
2020-09-11 11:39:25 +1000

    Submit challenge tasks: list

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // Buttons
-document.querySelector("#display-name-button").addEventListener('click', () => {
-    document.querySelector("#display-name").textContent = "Susanti";
-    document.querySelector("#display-name").classList.toggle('invisible');
+document.querySelector('#display-name-button').addEventListener('click', () => {
+    document.querySelector('#display-name').textContent = 'Susanti';
+    document.querySelector('#display-name').classList.toggle('invisible');
 });
 
 // Mouse Over
-let light = document.querySelector("#light-bulb");
+const light = document.querySelector('#light-bulb');
 
-light.onmouseenter = () => light.src = "images/light-bulb-on.png";
-light.onmouseleave = () => light.src = "images/light-bulb-off.png";
+light.onmouseenter = () => light.src = 'images/light-bulb-on.png';
+light.onmouseleave = () => light.src = 'images/light-bulb-off.png';
 
 // Toggle
-let toggleAlert = () => document.querySelector("#toggle-alert").classList.toggle('invisible');
+const toggleAlert = () => document.querySelector('#toggle-alert').classList.toggle('invisible');
 
 // Validate
 document.querySelector('#form-validate button').addEventListener('click', () => {
     event.preventDefault();
 
-    let input = document.querySelector("#form-validate-first-name");
+    const input = document.querySelector('#form-validate-first-name');
+
     if (input.value.length >= 2) {
         input.classList.add('is-valid');
         input.classList.remove('is-invalid');
@@ -29,7 +30,28 @@ document.querySelector('#form-validate button').addEventListener('click', () => 
 });
 
 // Challenge: Lists
-
+document.querySelector('#form-hobby').addEventListener('submit', () => {
+    const item = document.createElement('li');
+    item.innerText = document.querySelector('#form-hobby-text').value;
+    item.classList.add('list-group-item');
+     
+    document.querySelector('#hobby-list').appendChild(item);
+    document.querySelector('#form-hobby-text').value = "";
+ }, true);
 
 
 // Challenge: Lists (Part 2)
+let hobbyList2 = document.querySelector('#hobby-list-2');
+document.querySelector('#hobby-list-2').addEventListener('click', (event) => {
+    if (event.target.classList.contains('remove-hobby')) {
+        event.preventDefault();
+        const item = event.target.parentElement;
+        hobbyList2.removeChild(item);
+    }
+}, true);
+
+
+// if(window.addEventListener) {
+//     document.querySelector('#form-hobby').addEventListener('submit', addHobby, true);
+//     document.querySelector('#hobby-list-2').addEventListener('click', deleteHobby, true);
+// }
